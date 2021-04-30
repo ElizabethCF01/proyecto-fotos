f19cecda8a679b60c050707a2d89dc0489027414
Elizabeth Calzadilla Fernandez <elizabethcalzadillafernandez@gmail.com>
2021-04-30 07:19:15 +0200

    refactoring code

--- controladores/controladorOfertas/ControlOffers.java
+++ controladores/controladorOfertas/ControlOffers.java
@@ -1,24 +1,26 @@
 package controladorOfertas;
 
-import javax.swing.JFrame;
+import java.util.ArrayList;
 
 import ofertas.Oferta;
-import ofertasVistas.LaminaDeMarcoGO;
-import ofertasVistas.MarcoGestionarOfertas;
 
 public class ControlOffers {
 
-	public static void main(String[] args) {
+	
+	 
+	ArrayList <Oferta> ofertas;
+	
+	public ControlOffers() {
 		
-		MarcoGestionarOfertas m= new MarcoGestionarOfertas();
-		m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ofertas= new ArrayList<Oferta>();
 		
-          
+		ofertas.add(new Oferta("Fotos 5x8 ", 10));
+		ofertas.add(new Oferta("Fotos 10x12 ", 20));
+		ofertas.add(new Oferta("Fotos 15x20 ", 30));
 	}
-	public Oferta[] ofertas= {
-			
-			new Oferta("Offer ", 10), 
-			new Oferta("Offer ", 10),
-			new Oferta("Offer", 10)
-	  };
+	
+	public ArrayList<Oferta> getOffers(){
+		return ofertas;
+	}
+	
 }
